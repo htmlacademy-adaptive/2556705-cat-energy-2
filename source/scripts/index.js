253a0319c425e4bb8877373edfdc6ac0df51b792
Ivan-Kirillov <vanyusha_kirillov_03@mail.ru>
2025-02-04 20:01:59 +0300

    add header--opened, closed, nols + add js

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+let navMain = document.querySelector('.header');
+let navToggle = document.querySelector('.header__menu-button');
+
+navMain.classList.remove('header--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('header--closed')) {
+    navMain.classList.remove('header--closed')
+    navMain.classList.add('header--opened');
+  } else {
+    navMain.classList.add('header--closed');
+    navMain.classList.remove('header--opened');
+  }
+});
